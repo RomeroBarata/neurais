5066ab49c3fa4eca7773d9d040694163a05f51b8
RomeroBarata <romero.barata@gmail.com>
2015-05-25 22:58:30 +1000

    Write to a .csv file
    Write the training, validation and test sets to .csv files.

--- neurais.m
+++ neurais.m
@@ -10,3 +10,8 @@ data = data(:, 1:(end - 1));
 % Validation: 25%
 % Test: 25%
 [training, trainingclass, validation, validationclass, test, testclass] = splitData(data, class);
+
+% Write to a csv file
+csvwrite('mammography-consolidated-training.csv', [training, trainingclass])
+csvwrite('mammography-consolidated-validation.csv', [validation, validationclass])
+csvwrite('mammography-consolidated-test.csv', [test, testclass])
